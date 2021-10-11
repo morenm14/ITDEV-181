9567ced99ad3dd963df616ef9e440d9cd9e19e6c
morenm14 <morenm14@gmatc.matc.edu>
2021-10-11 12:58:48 -0500

    completed custom toast

--- Activity6/CustomToast/app/src/main/java/com/example/customtoast/MainActivity.java
+++ Activity6/CustomToast/app/src/main/java/com/example/customtoast/MainActivity.java
@@ -3,12 +3,42 @@ package com.example.customtoast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button showToastBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        showToastBtn = findViewById(R.id.showToastBtn);
+
+        showToastBtn.setOnClickListener(view -> {
+
+            Toast.makeText(this, "This is a toast", Toast.LENGTH_SHORT).show();
+            showMyToast("This is my toast message");
+        });
+    }
+
+    public void showMyToast(String message){
+
+        View view = getLayoutInflater().inflate(R.layout.my_custom_toast, (ViewGroup) findViewById(R.id.myToastLayout));
+
+        TextView toastMessage = view.findViewById(R.id.myToastMessage);
+        toastMessage.setText(message);
+
+        Toast myToast = new Toast(MainActivity.this);
+        myToast.setDuration(Toast.LENGTH_LONG);
+        myToast.setView(view);
+        myToast.setGravity(Gravity.BOTTOM | Gravity.FILL_HORIZONTAL, 0,200);
+        myToast.show();
     }
 }
