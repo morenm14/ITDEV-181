2c354daf6df03fe008f2e436f624596f8defb9b1
morenm14 <morenm14@gmatc.matc.edu>
2021-10-25 12:12:05 -0500

    permissions demo completed

---
+++ Activity8/PermissionsDemo/app/src/main/java/com/example/permissionsdemo/MainActivity.java
@@ -0,0 +1,79 @@
+package com.example.permissionsdemo;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Instrumentation;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.security.PrivateKey;
+
+public class MainActivity extends AppCompatActivity{
+    Button permissionBtn;
+    private static final int PERMISSION_REQUEST_CODE = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        permissionBtn = findViewById(R.id.permissionBtn);
+
+        permissionBtn.setOnClickListener(view -> {
+            //check if permission already exist
+            if (ContextCompat.checkSelfPermission(MainActivity.this,Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this, "Already Have Permission", Toast.LENGTH_SHORT).show();
+            }else{
+                //ask for permissions
+                ActivityCompat.requestPermissions(MainActivity.this, new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        //checks the request code
+        if(requestCode == PERMISSION_REQUEST_CODE){
+
+            //if the user checks allow
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
+
+            //if the user selects deny, it would ask again
+            }else if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)){
+
+                    //show the user when deny is clicked
+                    AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+                    alertDialog.setMessage("Please allow")
+                            .setTitle("Permission Required");
+                    alertDialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            ActivityCompat.requestPermissions(MainActivity.this, new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
+                        }
+                    });
+                    alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            Toast.makeText(MainActivity.this, "No Permission Granted", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+                    alertDialog.show();
+
+                }else{
+                    Toast.makeText(MainActivity.this, "No Permission Granted, We won't show this again.", Toast.LENGTH_SHORT).show();
+                }
+        }
+    }
+}
